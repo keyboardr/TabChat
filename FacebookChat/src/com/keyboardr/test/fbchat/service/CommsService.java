3e82903f47a590fc964167e74e932269530cc7ce
Keyboarder <Keyboarder@192.168.1.67>
2011-08-14 13:47:16 -0700

    comms service

--- FacebookChat/src/com/keyboardr/test/fbchat/service/CommsService.java
+++ FacebookChat/src/com/keyboardr/test/fbchat/service/CommsService.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
 
-import com.keyboardr.test.fbchat.BuddyList;
 import com.keyboardr.test.fbchat.structures.ChatListener;
 import com.keyboardr.test.fbchat.structures.CombAccount;
 import com.keyboardr.test.fbchat.structures.CombChat;
@@ -27,45 +25,7 @@ public class CommsService extends Service implements ChatListener {
 
 	private final IBinder mBinder = new CommsBinder();
 	private List<ChatListener> mChatListeners;
-
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		super.onStartCommand(intent, flags, startId);
-		if (intent.getAction().equals(ACTION_CONNECT)) {
-			final CombAccount account = BuddyList.getAccounts().get(
-					intent.getIntExtra(EXTRA_ACCOUNT, -1));
-			new AsyncTask<Void, Void, Void>() {
-
-				@Override
-				protected Void doInBackground(Void... arg0) {
-					account.connect();
-					return null;
-				}
-
-				@Override
-				protected void onPostExecute(Void result) {
-					account.addChatListener(CommsService.this);
-					BuddyList.requestUpdate();
-				}
-
-			}.execute();
-		}
-		if (intent.getAction().equals(ACTION_DISCONNECT)) {
-			final CombAccount account = BuddyList.getAccounts().get(
-					intent.getIntExtra(EXTRA_ACCOUNT, -1));
-			new AsyncTask<Void, Void, Void>() {
-
-				@Override
-				protected Void doInBackground(Void... arg0) {
-					account.disconnect();
-					return null;
-				}
-
-			}.execute();
-		}
-
-		return START_STICKY;
-	}
+	private List<CombAccount> mAccounts;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
